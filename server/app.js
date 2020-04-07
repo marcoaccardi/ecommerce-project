4ce7ca74b7ecd89448567e961ccc825493c7868e
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-07 11:46:49 +0200

    Init DB

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error : ${err.message}`);
+});
 
 app.get("/", (req, res) => {
   res.send("hello from node");

894eb14c896c30914c4d95dc4736092d20566175
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-08 10:12:41 +0200

    Add Express Validator

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -25,6 +26,7 @@ mongoose.connection.on("error", (err) => {
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 //routes middleware
 app.use("/api", userRoutes);
 

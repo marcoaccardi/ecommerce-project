ae319b249a436b3456b2f8fdab12205f382a26ab
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-11 12:27:56 +0200

    Add product update

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { create, productById, read, remove } = require("../controllers/product");
+const {
+  create,
+  productById,
+  read,
+  remove,
+  update,
+} = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -12,6 +18,13 @@ router.delete(
   isAuth,
   isAdmin,
   remove
+);
+router.put(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
 );
 
 router.param("productId", productById);

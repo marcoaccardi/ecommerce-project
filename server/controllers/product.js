821a4efd3aa7ed8f33a4e49d26d6bef1f8a68203
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-11 10:05:11 +0200

    Add product validation

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -13,9 +13,29 @@ exports.create = (req, res) => {
         error: "Image could not be uploaded",
       });
     }
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
     let product = new Product(fields);
 
     if (files.photo) {
+      console.log("FILES PHOTO", files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less then 1MB in size",
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
@@ -23,6 +43,7 @@ exports.create = (req, res) => {
       if (err) {
         return res.status(400).json({ error: errorHandler(err) });
       }
+      res.json(result);
     });
   });
 };

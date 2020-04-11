bc64daf060f4bfe4dde4ceecfb08732da48295d6
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-11 09:17:20 +0200

    Create product with file upload

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 mongoose
   .connect(process.env.DATABASE, {
@@ -32,6 +33,7 @@ app.use(expressValidator());
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {

a42317e93db95d43329e14d13fca1fb3da235638
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-12 11:34:39 +0200

    Add env variables

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Layout } from "../core/Layout";
+import { API } from "../config";
 
 const Signup = () => (
   <div>
     <Layout title="Signup" description="Signup to Node React e-commerce App">
-      ...
+      {API}
     </Layout>
   </div>
 );

403cb1711a19061d7b288bb44682a52acbfd7897
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-12 10:08:45 +0200

    Add npm cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const expressValidator = require("express-validator");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -29,6 +30,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 //routes middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);

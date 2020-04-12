f6e0940abe24234035e2cc6b23ba6eb268346561
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-12 18:27:12 +0200

    Add signup form with eventHandler

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Layout } from "../core/Layout";
-
+import { API } from "../config";
 const Signin = () => (
   <div>
     <Layout title="Signin" description="Signin to Node React e-commerce App">
-      ...
+      {API}
     </Layout>
   </div>
 );

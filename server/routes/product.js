d4e019ea3038c2ed5d728bc2f865bb0a54aed0d7
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-12 09:36:18 +0200

    Add get product photo

--- server/routes/product.js
+++ server/routes/product.js
@@ -10,6 +10,7 @@ const {
   listRelated,
   listCategories,
   listBySearch,
+  photo,
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -34,6 +35,7 @@ router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
+router.get("product/photo/:productId", photo);
 
 router.param("productId", productById);
 router.param("userId", userById);

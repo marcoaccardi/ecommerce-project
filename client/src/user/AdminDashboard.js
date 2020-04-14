175f291a78b98ceee2458a5f4db48debcb9ea56e
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-14 09:55:08 +0200

    Fix link adminDashboard

--- client/src/user/AdminDashboard.js
+++ client/src/user/AdminDashboard.js
@@ -14,7 +14,7 @@ const AdminDashboard = () => {
         <h4 className="card-header">Admin Links</h4>
         <ul className="list-group">
           <li className="list-group-item">
-            <Link className="nav-link" to="create/category">
+            <Link className="nav-link" to="/create/category">
               Create Category
             </Link>
           </li>

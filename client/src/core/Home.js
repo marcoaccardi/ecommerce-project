c3ee3c855380b027250593f28708f4f6653dbc72
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-17 09:25:31 +0200

    Add show product in card

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Layout } from "./Layout";
 import { getProducts } from "./apiCore";
+import { Card } from "./Card";
 
 const Home = () => {
   const [productsBySell, setProductsBySell] = useState([]);
@@ -34,9 +35,18 @@ const Home = () => {
 
   return (
     <Layout title="Home Page" description="Node React e-commerce App">
-      {JSON.stringify(productsByArrival)}
-      <hr />
-      {JSON.stringify(productsBySell)}
+      <h2 className="mb-4">New Arrivals</h2>
+      <div className="row">
+        {productsByArrival.map((product, index) => (
+          <Card key={index} product={product} />
+        ))}
+      </div>
+      <h2 className="mb-4">Best Sellers</h2>
+      <div className="row">
+        {productsBySell.map((product, index) => (
+          <Card key={index} product={product} />
+        ))}
+      </div>
     </Layout>
   );
 };

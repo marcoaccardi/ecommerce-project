f3303da81997fc7a5ab861dc6a49cc586ae45eb6
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-17 09:00:55 +0200

    Add sort products by sold and arrival

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Layout } from "./Layout";
+import { getProducts } from "./apiCore";
 
-const Home = () => (
-  <Layout title="Home Page" description="Node React e-commerce App" />
-);
+const Home = () => {
+  const [productBySell, setProductBySell] = useState([]);
+  const [productByArrival, setProductByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductBySell = () => {
+    getProducts("sold").then((data) => {
+      // console.log(data);
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductBySell(data);
+      }
+    });
+  };
+  const loadProductByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductByArrival();
+    loadProductBySell();
+  });
+
+  return (
+    <Layout title="Home Page" description="Node React e-commerce App">
+      {JSON.stringify(productByArrival)}
+      {JSON.stringify(productBySell)}
+    </Layout>
+  );
+};
 
 export { Home };

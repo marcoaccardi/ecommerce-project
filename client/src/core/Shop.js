1310d3b93bfa291de39e95efd0e68495ba02d7d6
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-18 12:44:20 +0200

    Add set filters with category

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -5,6 +5,12 @@ import { getCategories } from "./apiCore";
 import { Checkbox } from "./Checkbox";
 
 const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: {
+      category: [],
+      price: [],
+    },
+  });
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
 
@@ -23,7 +29,10 @@ const Shop = () => {
   }, []);
 
   const handleFilters = (filters, filterBy) => {
-    console.log("SHOP", filters, filterBy);
+    // console.log("SHOP", filters, filterBy);
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
   };
 
   return (
@@ -42,7 +51,7 @@ const Shop = () => {
             />
           </ul>
         </div>
-        <div className="col-8">right sidebar</div>
+        <div className="col-8">{JSON.stringify(myFilters)}</div>
       </div>
     </Layout>
   );

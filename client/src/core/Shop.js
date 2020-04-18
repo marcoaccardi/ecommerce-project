83d02988fd49842cc25c297d619a136939aefef8
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-18 11:34:01 +0200

    add show categories in shop side bar

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Layout } from "./Layout";
 import { Card } from "./Card";
 import { getCategories } from "./apiCore";
+import { Checkbox } from "./Checkbox";
 
 const Shop = () => {
   const [categories, setCategories] = useState([]);
@@ -22,10 +23,18 @@ const Shop = () => {
   }, []);
 
   return (
-    <Layout title="Shop" description="Search and find book of your choice">
+    <Layout
+      title="Shop"
+      description="Search and find book of your choice"
+      className="container-fluid"
+    >
       <div className="row">
-        <div className="col-4">left sidebar</div>
-        {JSON.stringify(categories)}
+        <div className="col-4">
+          <h4>Filter by categories</h4>
+          <ul>
+            <Checkbox categories={categories} />
+          </ul>
+        </div>
         <div className="col-8">right sidebar</div>
       </div>
     </Layout>

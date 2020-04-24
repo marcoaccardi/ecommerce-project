6dbc19fd2fdc9fe719e0ac2e49d830aa732bd0c9
Marco Accardi <contact.marco.accardi@gmail.com>
2020-04-24 08:24:59 +0200

    add load more button and logicin shop component

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -17,6 +17,7 @@ const Shop = () => {
   const [error, setError] = useState(false);
   const [limit, setLimit] = useState(6);
   const [skip, setSkip] = useState(0);
+  const [size, setSize] = useState(0);
   const [filteredResults, setFilteredResults] = useState([]);
 
   const init = () => {
@@ -36,8 +37,34 @@ const Shop = () => {
         setError(data.error);
       } else {
         setFilteredResults(data.data);
+        setSize(data.size);
+        setSkip(0);
       }
     });
+  };
+
+  const loadMore = () => {
+    let toSkip = skip + limit;
+    getFilteredProducts(toSkip, limit, myFilters.filters).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults([...filteredResults, ...data.data]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    });
+  };
+
+  const loadMoreButton = () => {
+    return (
+      size > 0 &&
+      size >= limit && (
+        <button onClick={loadMore} className="btn btn-warning mb-5">
+          Load more
+        </button>
+      )
+    );
   };
 
   useEffect(() => {
@@ -102,6 +129,8 @@ const Shop = () => {
               <Card key={i} product={product} />
             ))}
           </div>
+          <hr />
+          {loadMoreButton()}
         </div>
       </div>
     </Layout>
